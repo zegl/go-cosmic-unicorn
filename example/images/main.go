4f032d27d8566886c3866cc9fcedcc4d4dce359f
Gustav Westling <gustav@westling.dev>
2024-02-03 11:20:30 +0100

    Working on 8 bit color support

--- example/images/main.go
+++ example/images/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"embed"
+	"fmt"
+	"math"
+	"time"
 
 	cosmic "github.com/zegl/go-cosmic-unicorn"
 
@@ -12,9 +15,16 @@ import (
 	"machine"
 )
 
+type Effect interface {
+	Init(ps PixelSetter)
+	Draw(ps PixelSetter)
+}
+
 func main() {
 	cu := cosmic.CosmicUnicorn{}
 	cu.Init()
+
+	var effect Effect
 
 	var fileIdx int
 	files := []string{"polar.png", "zegl.png", "mario_box.png", "gopher.png", "bread.png", "pepper.png", "pizza.png", "popcorn.png", "soda.png", "taco.png", "watermelon.png"}
@@ -23,17 +33,54 @@ func main() {
 	var pressedA = false
 	handleA := func() {
 		fileIdx = (fileIdx + 1) % len(files)
-		drawImage(files[fileIdx], cu.SetColor)
+		effect = &Image{name: files[fileIdx]}
+		effect.Init(cu.SetColor)
+		// drawImage(files[fileIdx], cu.SetColor)
 	}
 	cosmic.SWITCH_A.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
 	cosmic.SWITCH_A.SetInterrupt(machine.PinFalling, func(p machine.Pin) {
 		pressedA = true
 	})
 
+	// Press B to draw gradient
+	var pressedB = false
+	handleB := func() {
+		// drawGradient(cu.SetColor)
+		effect = &Gradient{}
+		effect.Init(cu.SetColor)
+	}
+	cosmic.SWITCH_B.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+	cosmic.SWITCH_B.SetInterrupt(machine.PinFalling, func(p machine.Pin) {
+		pressedB = true
+	})
+
+	// Press C to draw circle
+	var pressedC = false
+	handleC := func() {
+		effect = &Circle{}
+		effect.Init(cu.SetColor)
+	}
+	cosmic.SWITCH_C.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+	cosmic.SWITCH_C.SetInterrupt(machine.PinFalling, func(p machine.Pin) {
+		pressedC = true
+	})
+
+	// Press D to draw fire
+	var pressedD = false
+	handleD := func() {
+		effect = &Fire{}
+		effect.Init(cu.SetColor)
+	}
+	cosmic.SWITCH_D.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+	cosmic.SWITCH_D.SetInterrupt(machine.PinFalling, func(p machine.Pin) {
+		pressedD = true
+	})
+
 	// Increase brightness
 	var pressedBrightnessUp = false
 	handleBrightnessUp := func() {
 		cu.ChangeBrightness(10)
+		// drawImage(files[fileIdx], cu.SetColor)
 	}
 	cosmic.SWITCH_BRIGHTNESS_UP.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
 	cosmic.SWITCH_BRIGHTNESS_UP.SetInterrupt(machine.PinFalling, func(p machine.Pin) {
@@ -44,6 +91,7 @@ func main() {
 	var pressedBrightnessDown = false
 	handleBrightnessDown := func() {
 		cu.ChangeBrightness(-10)
+		// drawImage(files[fileIdx], cu.SetColor)
 	}
 	cosmic.SWITCH_BRIGHTNESS_DOWN.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
 	cosmic.SWITCH_BRIGHTNESS_DOWN.SetInterrupt(machine.PinFalling, func(p machine.Pin) {
@@ -51,12 +99,35 @@ func main() {
 	})
 
 	// Draw default image
-	drawImage(files[fileIdx], cu.SetColor)
+	// drawImage(files[fileIdx], cu.SetColor)
+	effect = &Fire{}
+	effect.Init(cu.SetColor)
+	effect.Draw(cu.SetColor)
+
+	var t uint8
+	t0 := time.Now()
 
 	for {
+		t++
+
 		if pressedA {
 			pressedA = false
 			handleA()
+		}
+
+		if pressedB {
+			pressedB = false
+			handleB()
+		}
+
+		if pressedC {
+			pressedC = false
+			handleC()
+		}
+
+		if pressedD {
+			pressedD = false
+			handleD()
 		}
 
 		if pressedBrightnessUp {
@@ -69,7 +140,19 @@ func main() {
 			handleBrightnessDown()
 		}
 
+		if t%4 == 0 {
+			effect.Draw(cu.SetColor)
+		}
+
+		// Draw to screen
 		cu.Draw()
+
+		if t == 100 {
+			dur := time.Since(t0)
+			fmt.Printf("Rendered 100 frames in %s / %f FPS\n", dur, 100/dur.Seconds())
+			t0 = time.Now()
+			t = 0
+		}
 	}
 }
 
@@ -78,8 +161,12 @@ var content embed.FS
 
 type PixelSetter func(x, y int, c color.Color)
 
-func drawImage(name string, ps PixelSetter) {
-	file, err := content.Open("assets/" + name)
+type Image struct {
+	name string
+}
+
+func (g *Image) Init(ps PixelSetter) {
+	file, err := content.Open("assets/" + g.name)
 	if err != nil {
 		log.Println(err)
 		return
@@ -97,4 +184,63 @@ func drawImage(name string, ps PixelSetter) {
 			ps(x, y, px)
 		}
 	}
+}
+
+func (g *Image) Draw(ps PixelSetter) {
+
+}
+
+type Gradient struct{}
+
+func (g *Gradient) Init(ps PixelSetter) {
+	for y := 0; y < 32; y++ {
+		for x := 0; x < 32; x++ {
+			ps(x, y, color.RGBA{
+				R: uint8(float32(x) * 255 / 32),
+				G: uint8(float32(y) * 255 / 32),
+			})
+		}
+	}
+}
+func (g *Gradient) Draw(ps PixelSetter) {
+
+}
+
+type Circle struct{}
+
+func (g *Circle) Init(ps PixelSetter) {
+	var t float64 = 0
+	center := 15
+	var radius float64 = 15
+
+	for y := 0; y < 32; y++ {
+		for x := 0; x < 32; x++ {
+			c := float64(y*32 + x)
+			dx := abs(center - x)
+			dy := abs(center - y)
+
+			dist := math.Sqrt(math.Pow(float64(dy), 2) + math.Pow(float64(dx), 2))
+
+			if dist < radius {
+				ps(x, y, color.RGBA{
+					R: uint8(100 + 50*math.Cos(c)),
+					G: uint8(100 + 50*math.Sin(c)),
+					B: uint8(100 + 50*math.Sin(c+t)),
+				})
+
+			} else {
+				ps(x, y, color.RGBA{})
+			}
+		}
+	}
+}
+func (g *Circle) Draw(ps PixelSetter) {
+
+}
+
+func abs(i int) int {
+	if i < 0 {
+		return i * -1
+	}
+	return i
 }
